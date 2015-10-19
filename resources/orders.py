e288beb483d7dedab8bfef7ff2baf22f382a7917
Paul Saumets <psaumets@gmail.com>
2015-10-19 11:45:18 -0400

    cleaned up order creation. added file writing of all orders and customers created.

--- resources/orders.py
+++ resources/orders.py
@@ -11,12 +11,18 @@ class Orders:
     def __init__(self, limit_sample_size):
 
         self.products = shopify.Product.find(limit=limit_sample_size)
+
         return
 
     def generate(self, number_products):
 
-        print("Total Products: {}".format(len(self.products)))
+        # these lists will be added to a data file after creation
+        orders_created = []
+        customers_created = []
 
+        print("Total Products Sampling From: {}\n".format(len(self.products)))
+
+        # create our factory for generating fake customer names
         fake = Factory.create('en_CA')
 
         for counter in range(number_products):
@@ -28,9 +34,21 @@ class Orders:
 
             success = new_order.save()
 
+            if success:
+                orders_created.append(str(new_order.id))
+                customers_created.append(str(new_order.customer.id))
+
             if new_order.errors:
                 # something went wrong!
                 print(new_order.errors.full_messages())
+
+        # Write our created data to file. This is required for simple deletion later using this same tool.
+        # If these files do not exist, you will have to delete the data manually through the Shopify dashboard.
+        with open('sdg-orders.csv', mode='a', encoding='utf-8') as order_file:
+            order_file.write('\n'.join(orders_created) + '\n')
+
+        with open('sdg-customers.csv', mode='a', encoding='utf-8') as customers_file:
+            customers_file.write('\n'.join(customers_created) + '\n')
 
         return
 
@@ -43,7 +61,7 @@ class Orders:
 
         # get a random # of products (aka line_items) for this purchase.
         products = random.sample(self.products, sample_size)
-        print("Total Random Products Grabbed: {}\n".format(len(products)))
+        print("Total Products Purchased In Order: {}\n".format(len(products)))
 
         for product in products:
             if len(product.variants) < int(settings['MAX_VARIANTS']):
