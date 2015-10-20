467800a0cdf28a6ebf7e28bcf2e980c692921cee
Paul Saumets <psaumets@gmail.com>
2015-10-20 14:38:11 -0400

    cleanup.

--- resource/orders.py
+++ resource/orders.py
@@ -3,8 +3,8 @@ import shopify
 
 import config
 
-from faker import Factory
-
+from resource.customers import Customers
+from pyactiveresource.connection import ResourceNotFound
 
 class Orders(object):
     def __init__(self, limit_sample_size=250):
@@ -22,14 +22,11 @@ class Orders(object):
 
         print("Total Products Sampling From: {}\n".format(len(self.products)))
 
-        # create our factory for generating fake customer names
-        fake = Factory.create('en_CA')
-
         for counter in range(number_orders):
 
-            print("Generating Order: " + str(counter))
+            print("Generating Order: {0}".format(str(counter + 1)))
             new_order = shopify.Order()
-            new_order.customer = dict(first_name=fake.first_name(), last_name=fake.last_name())
+            new_order.customer = Customers.generate_data()
             new_order.line_items = self.line_items_create()
 
             success = new_order.save()
@@ -40,7 +37,9 @@ class Orders(object):
 
             if new_order.errors:
                 # something went wrong!
-                print(new_order.errors.full_messages())
+                # TODO: we need to loop over our error messages and print them
+                #print(new_order.errors.full_messages())
+                return
 
         # Write our created data to file. This is required for simple deletion later using this same tool.
         # If these files do not exist, you will have to delete the data manually through the Shopify dashboard.
@@ -84,20 +83,20 @@ class Orders(object):
         if orders is None:
             # delete all orders
             with open('sdg-orders.csv') as order_file:
-                orders_delete = order_file.readlines()
+                orders_delete = order_file.read().splitlines()
 
             for order_number in orders_delete:
 
                 try:
-                    print("Finding order #: {}".format(order_number))
+                    print("Finding order #: {0}".format(order_number))
                     order = shopify.Order.find(int(order_number))
-                    print("Attempting to cancel order #: {}".format(order_number), end='')
+                    print("Attempting to cancel order #: {0}".format(order_number))
                     order.cancel()
-                    print("Order #{} cancelled.".format(order_number))
-                    print("Attempting to delete order #: {}".format(order_number), end='')
+                    print("Order #{0} cancelled.".format(order_number))
+                    print("Attempting to delete order #: {0}".format(order_number))
                     order.destroy()
-                    print("Order #{} deleted.\n".format(order_number))
-                except:
-                    print("Order #: {} not found.".format(order_number))
+                    print("[DELETED] Order #{0}\n".format(order_number))
+                except ResourceNotFound:
+                    print("[WARNING] Order #{0} not found.".format(order_number))
 
         return
